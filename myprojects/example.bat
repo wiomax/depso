303f027e257aa533bff6b8895b1c3314e981db71
Xiao-Feng Xie <xie@wiomax.com>
2015-09-28 19:25:20 -0400

    add myprojects/example.bat

---
+++ myprojects/example.bat
@@ -0,0 +1,3 @@
+java -jar ../dist/depso.jar DEPSO Problem=problem.constrained.Michalewicz_G1 N=70 T=2000 Tout=100
+java -jar ../dist/depso.jar DEPSO Problem=problem.constrained.Michalewicz_G3 isACR=true  N=70 T=2000 Tout=100
+java -jar ../dist/depso.jar DEPSO T=100 N=10 Tout=10 Problem=problem.unconstrained.GoldsteinPrice 
