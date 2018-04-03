807f6df78777331a9a7112dc5c4ce8b9a390f097
Xiao-Feng Xie <xie@wiomax.com>
2018-04-03 12:14:22 -0400

    None

--- myprojects/example.bat
+++ myprojects/example.bat
@@ -1,3 +1,8 @@
-java -jar ../dist/depso.jar DEPSO Problem=problem.constrained.Michalewicz_G1 N=70 T=2000 Tout=100
-java -jar ../dist/depso.jar DEPSO Problem=problem.constrained.Michalewicz_G3 isACR=true  N=70 T=2000 Tout=100
-java -jar ../dist/depso.jar DEPSO T=100 N=10 Tout=10 Problem=problem.unconstrained.GoldsteinPrice 
+#Optimization examples
+java -jar ../dist/depso.jar Problem=problem.constrained.Michalewicz_G1 N=70 T=2000 Tout=100
+java -jar ../dist/depso.jar Problem=problem.constrained.Michalewicz_G3 N=70 T=2000 Tout=100 isACR=true
+java -jar ../dist/depso.jar Problem=problem.unconstrained.GoldsteinPrice N=10 T=100 Tout=10 
+
+#Simulation examples
+#Params: [Function] [Input Variables (Separated by ",")]
+java -cp ../dist/depso.jar problem.Simulator problem.constrained.Michalewicz_G1 "1,1,1,1,1,1,1,1,1,3,3,3,1"
